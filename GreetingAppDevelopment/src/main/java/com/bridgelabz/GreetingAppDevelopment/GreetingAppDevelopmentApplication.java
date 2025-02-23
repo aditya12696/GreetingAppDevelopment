dffc657faf163cd309481a6c2dbce5395bacbc8b
aditya12696 <aditysharma127@gmail.com>
2025-02-23 13:06:27 +0530

    Aditya Add Program for App to DataBase for Greeting Messages in the Repository using SpringBoot

--- GreetingAppDevelopment/src/main/java/com/bridgelabz/GreetingAppDevelopment/GreetingAppDevelopmentApplication.java
+++ GreetingAppDevelopment/src/main/java/com/bridgelabz/GreetingAppDevelopment/GreetingAppDevelopmentApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GreetingAppDevelopmentApplication {
 
 	public static void main(String[] args) {
-		// enter point of app Spring Boot
+		// enter point of app Spring Boot using Data Base
 		SpringApplication.run(GreetingAppDevelopmentApplication.class, args);
 	}
 
